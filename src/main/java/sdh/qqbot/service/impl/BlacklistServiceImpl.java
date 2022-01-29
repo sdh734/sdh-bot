fba520bc8ecbf0333d6fbae1145ad8d495e477c8
SDH <sdh734@qq.com>
2022-01-30 02:43:18 +0800

    添加SQL文件。

--- src/main/java/sdh/qqbot/service/impl/BlacklistServiceImpl.java
+++ src/main/java/sdh/qqbot/service/impl/BlacklistServiceImpl.java
@@ -1,15 +1,13 @@
 package sdh.qqbot.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 import sdh.qqbot.dao.Blacklist;
 import sdh.qqbot.mapper.BlacklistMapper;
 import sdh.qqbot.service.IBlacklistService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
 
 /**
- * <p>
  *  服务实现类
- * </p>
  *
  * @author SDH
  * @since 2022-01-08
--- src/main/java/sdh/qqbot/service/impl/UserServiceImpl.java
+++ src/main/java/sdh/qqbot/service/impl/UserServiceImpl.java
@@ -1,15 +1,13 @@
 package sdh.qqbot.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 import sdh.qqbot.dao.User;
 import sdh.qqbot.mapper.UserMapper;
 import sdh.qqbot.service.IUserService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
 
 /**
- * <p>
  *  服务实现类
- * </p>
  *
  * @author SDH
  * @since 2022-01-08
